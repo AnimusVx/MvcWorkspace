ec4c05179aba192c7ac97857a4d55ec825e8b84e
AnimusVx <ulucmrce@gmail.com>
2022-12-16 14:06:35 +0300

    Index, Delete Actions Have been completed. Expense Controller has been added

--- MvcWorkspace/Controllers/ExpenseController.cs
+++ MvcWorkspace/Controllers/ExpenseController.cs
@@ -1,12 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MvcWorkspace.Data;
+using MvcWorkspace.Models;
 
 namespace MvcWorkspace.Controllers
 {
     public class ExpenseController : Controller
     {
+        private readonly AppDbContext _db;
+
+        public ExpenseController(AppDbContext db)
+        { 
+          _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Expense> expenses  = _db.Expenses;
+            return View(expenses);
+        }
+        public IActionResult Delete(int id) 
+        {
+            var expense = _db.Expenses.Find(id);
+            if(expense == null || id == 0)
+            {
+                ViewData["Title"] = "hata";
+                return NotFound();
+            }
+            ViewData["Title"] = "hata değil";
+
+            _db.Expenses.Remove(expense);
+            _db.SaveChanges ();
+            return RedirectToAction("Index");
         }
     }
 }
