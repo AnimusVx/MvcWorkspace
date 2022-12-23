34b595d01471e4230bbbed6d8a80b13f9d6f2310
AnimusVx <ulucmrce@gmail.com>
2022-12-23 15:11:02 +0300

    TestM Controller added. TestM created. Image file path adding were applied. View fixed.

---
+++ MvcWorkspace/Controllers/TestMController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MvcWorkspace.Models;
+
+namespace MvcWorkspace.Controllers
+{
+    public class TestMController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiforgeryToken]
+        public IActionResult Upsert(TestM testM, IFormFile file)
+        {
+            if(!ModelState.IsValid)
+            {
+                fileName = 
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
